cec0ff0e70069527a8408e53dd4f6731449422e6
xxHMT <algoroku@gmail.com>
2025-07-23 22:19:33 +0700

    BaiKiemTraKy_07_2025

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BaiKiemTraKy072025.Models;
+
+namespace BaiKiemTraKy072025.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Data/ApplicationDbContex.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using BaiKiemTraKy072025.Models;
+
+namespace BaiKiemTraKy072025.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+        { }
+        
+    }
+}
